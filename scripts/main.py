81415435aeb585a4a135cbcbfa006c0c91262152
Jose.Manuel.Magana.Arias <Jose.Manuel.Magana.Arias@es.ey.com>
2023-01-08 07:11:16 +0100

    Load and display data

--- scripts/main.py
+++ scripts/main.py
@@ -4,50 +4,53 @@ import base64
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import os
 
-st.title('Precios Unitarios del Bajio')
+st.title('Precios Unitarios del Bajío')
 
 st.markdown("""
 #### **¡Bienvenido!** 
-#### Base de datos de Precios Unitarios de materiales de construcción del Bajio Mexicano
+#### Base de datos de Precios Unitarios de materiales de construcción del Bajío Mexicano
 """)
 
 st.sidebar.header('Selección de datos')
 selected_year = st.sidebar.selectbox('Año', list(reversed(range(2020,2023))))
 
+def get_root_dir():
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    root_dir = os.path.dirname(script_path)
+    # root_dir = os.path.dirname(script_folder)
+    return root_dir
+root_dir_path = get_root_dir()
+
 # Carga de datos
 @st.cache
-def load_data(year):
-    url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
-    html = pd.read_html(url, header = 0)
-    df = html[0]
-    raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
-    raw = raw.fillna(0)
-    playerstats = raw.drop(['Rk'], axis=1)
-    return playerstats
-playerstats = load_data(selected_year)
+def load_csv(filepath):
+    df = pd.read_csv(filepath, sep=';')
+    return df
+source_df = load_csv(root_dir_path+'\data\data.csv')
 
 # Sidebar - State selection
-sorted_unique_state = ['Guanajuato','Michoacan','Queretaro'] #sorted(playerstats.Tm.unique())
-selected_state = st.sidebar.multiselect('Seleccione el Estado', sorted_unique_state, sorted_unique_state)
+sorted_unique_state = sorted(source_df.Estado.unique())
+selected_state = st.sidebar.multiselect('Estado', sorted_unique_state, sorted_unique_state)
 
 # Sidebar - Category selection
-unique_category = ['Acero y Metal','Cementos y Pegazulejo','Block y Ladrillo','Plomería']
+unique_category = ['Acero y Metal','Cementos y Pegazulejo','Block y Ladrillo','Maquinaria y Equipo']
 selected_category = st.sidebar.multiselect('Materiales', unique_category, unique_category)
 
 # Filtering data
-df_selected_team = playerstats[(playerstats.Tm.isin(selected_state)) & (playerstats.Pos.isin(selected_category))]
+df_selected = source_df[(source_df.Estado.isin(selected_state)) & (source_df.Categoria.isin(selected_category))]
 
-st.header('Tabla de precios y materiales')
-st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
-st.dataframe(df_selected_team)
+st.header('Tabla de precios, materiales y proveedores')
+st.write('Productos seleccionados: ' + str(df_selected.shape[0]))
+st.dataframe(df_selected)
 
-# Download NBA player stats data
+# Download selected data
 # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
 def filedownload(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Descargar datos</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="unit_prices_download.csv">Descargar datos</a>'
     return href
 
-st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
+st.markdown(filedownload(df_selected), unsafe_allow_html=True)
