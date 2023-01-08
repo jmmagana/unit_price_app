013b83526541709fb1481d8cc9ee630e722de202
Jose.Manuel.Magana.Arias <Jose.Manuel.Magana.Arias@es.ey.com>
2023-01-08 07:32:03 +0100

    Add Supplier filter

--- scripts/main.py
+++ scripts/main.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import numpy as np
 import os
 
+st.set_page_config(layout="wide")  # this needs to be the first Streamlit command
 st.title('Precios Unitarios del Bajío')
 
 st.markdown("""
@@ -13,13 +14,9 @@ st.markdown("""
 #### Base de datos de Precios Unitarios de materiales de construcción del Bajío Mexicano
 """)
 
-st.sidebar.header('Selección de datos')
-selected_year = st.sidebar.selectbox('Año', list(reversed(range(2020,2023))))
-
 def get_root_dir():
     script_path = os.path.dirname(os.path.realpath(__file__))
     root_dir = os.path.dirname(script_path)
-    # root_dir = os.path.dirname(script_folder)
     return root_dir
 root_dir_path = get_root_dir()
 
@@ -30,16 +27,30 @@ def load_csv(filepath):
     return df
 source_df = load_csv(root_dir_path+'\data\data.csv')
 
+# Sidebar
+st.sidebar.header('Selección de datos')
+
+# Sidebar - State selection
+# selected_year = st.sidebar.selectbox('Año', list(reversed(range(2020,2023))))
+
 # Sidebar - State selection
 sorted_unique_state = sorted(source_df.Estado.unique())
 selected_state = st.sidebar.multiselect('Estado', sorted_unique_state, sorted_unique_state)
+
+# Sidebar - Supplier selection
+sorted_unique_supplier = sorted(source_df.Proveedor.unique())
+selected_supplier = st.sidebar.multiselect('Proveedor', sorted_unique_supplier, sorted_unique_supplier)
 
 # Sidebar - Category selection
 unique_category = ['Acero y Metal','Cementos y Pegazulejo','Block y Ladrillo','Maquinaria y Equipo']
 selected_category = st.sidebar.multiselect('Materiales', unique_category, unique_category)
 
 # Filtering data
-df_selected = source_df[(source_df.Estado.isin(selected_state)) & (source_df.Categoria.isin(selected_category))]
+df_selected = source_df[
+    (source_df.Estado.isin(selected_state)) &
+    (source_df.Proveedor.isin(selected_supplier)) &
+    (source_df.Categoria.isin(selected_category))
+    ]
 
 st.header('Tabla de precios, materiales y proveedores')
 st.write('Productos seleccionados: ' + str(df_selected.shape[0]))
